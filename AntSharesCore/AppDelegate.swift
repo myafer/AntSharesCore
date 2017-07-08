35652aadb7bd821d332ff8160e328b70f4c4183e
afer <afer@AferdeMacBook-Pro.local>
2017-07-08 14:56:58 +0800

    init

---
+++ AntSharesCore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AntSharesCore
+//
+//  Created by afer on 2017/7/8.
+//  Copyright © 2017年 afer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AntSharesCore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AntSharesCore
+//
+//  Created by afer on 2017/7/8.
+//  Copyright © 2017年 afer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
