f78c5ba694afc8411c4628bdbfc925fbdd064f84
afer <aferios@163.com>
2017-07-09 12:25:49 +0800

    icon

--- AntSharesCore/AppDelegate.swift
+++ AntSharesCore/AppDelegate.swift
@@ -23,6 +23,68 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func signAction(_ sender: Any) {
+    }
+    @IBAction func extractANCAction(_ sender: Any) {
+    }
+    
 
 }
 
+extension AppDelegate {
+    
+    //----- wallet menu
+    
+    // 创建钱包数据库
+    
+    @IBAction func createWalletAction(_ sender: Any) {
+        print(#function)
+    }
+    
+    // 打开钱包数据库
+    
+    @IBAction func openWalletAction(_ sender: Any) {
+        print(#function)
+    }
+    
+    // 修改密码
+    
+    @IBAction func changeThePasswordsAction(_ sender: Any) {
+    }
+    
+    // 重建钱包索引
+    
+    @IBAction func resetWalletIndexAction(_ sender: Any) {
+    }
+    
+    // 还原账户
+    
+    @IBAction func resetTheAccountAction(_ sender: Any) {
+        
+    }
+    
+    // --- trade menu
+    
+    // 转账
+    
+    @IBAction func giroAction(_ sender: Any) {
+    
+    }
+    
+    // 交易
+    
+    @IBAction func tradeAction(_ sender: Any) {
+    
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
